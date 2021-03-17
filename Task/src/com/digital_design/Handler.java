7f0976a7b9b0f1c50369a8a62a6b8fe61d8ff6ee
ArmenYuKh <armencj@yandex.ru>
2021-03-17 20:49:15 +0300

    Simplifying validation method and fixing errors

--- Task/src/com/digital_design/Handler.java
+++ Task/src/com/digital_design/Handler.java
@@ -42,70 +42,38 @@ class Handler {
         // 4) не может быть ничего кроме цифр, латинских букв и скобок
         // 5) скобки не могут быть пустые
         // 6) внутри повторяющейся подстроки не могут быть исключительно цифры
+        // 7) скобки всегда есть
 
-        boolean numBracket = true; // для определения того, после числа повторений всегда ли идет '['
-        boolean bracketCnt = false; // для определения равенства количества скобок
-        boolean letterBracket = false; // для определения того, не идёт ли после латинских букв '['
-        boolean validCharacters = false; // для опредлеения допустимых символов на вход
-        boolean emptyBrackets = false; // для определения того, не пустое ли выражение в квадратных скобках
-        boolean onlyDigits = false; // для определения того, что не исключительно цифры внутри квадратных скобок
-
-        boolean valid = false; // для определения валидности строки с учётом всех критериев
 
         // Поиск и выбор подстроки, заданной шаблоном
-        // определяем, исключительно ли цифры внутри квадратных скобок
-        String regex1 = "\\[\\d+]";
-        Pattern pattern1 = Pattern.compile(regex1);
-        Matcher matcher1 = pattern1.matcher(str);
-        while (matcher1.find()) {
-            onlyDigits = true;
-        }
-
-        // определяем, всегда ли после числа повторений идёт '[' (идём от противного, т.е. проверяем, что нет выражений типа
+        // 1) определяем, исключительно ли цифры внутри квадратных скобок
+        // 2) определяем, всегда ли после числа повторений идёт '[' (идём от противного, т.е. проверяем, что нет выражений типа
         // 'числоБуква', 'число]' или число на конце строки)
-        String regex2 = "(\\d+[a-zA-Z]+)|(\\d+]+)|(\\d$+)";
-        Pattern pattern2 = Pattern.compile(regex2);
-        Matcher matcher2 = pattern2.matcher(str);
-        while (matcher2.find()) {
-            numBracket = false;
-        }
-
-        // определяем, идёт ли после латинских букв '['
-        String regex3 = "[a-zA-Z]+\\[";
-        Pattern pattern3 = Pattern.compile(regex3);
-        Matcher matcher3 = pattern3.matcher(str);
-        while (matcher3.find()) {
-            letterBracket = true;
-        }
-
-        // опредлеяем, не пустое ли выражение в квадратных скобках
-        String regex4= "\\[]";
-        Pattern pattern4 = Pattern.compile(regex4);
-        Matcher matcher4 = pattern4.matcher(str);
-        while (matcher4.find()) {
-            emptyBrackets = true;
-        }
-        int balance = 0; // количество скобок
-        int tmp;
+        // 3) определяем, идёт ли после латинских букв '['
+        // 4) опредлеяем, не пустое ли выражение в квадратных скобках
+        String regex = "(\\[\\d+])|(\\d+[a-zA-Z]+)|(\\d+]+)|(\\d$+)|([a-zA-Z]+\\[)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.find())
+            return false;
+        int balance = 0; // разность открывающих и закрывающих скобок
+        int bracketCnt = 0; // количество скобок
         char[] arr = str.toCharArray();
         // определяем все допустимые символы на вход
         for (char c : arr) {
-            if (Character.isLetter(c) || Character.isDigit(c) || c == '[' || c == ']') {
-                validCharacters = true;
+            if ((!Character.isLetter(c)) && (!Character.isDigit(c)) && (c != '[') && (c != ']')) {
+                return false;
             }
-            if (c == '[')
+            if (c == '[') {
                 balance++;
-            if (c == ']')
+                bracketCnt++;
+            }
+            if (c == ']') {
                 balance--;
+                bracketCnt++;
+            }
         }
-
-        // определяем равенство количества открывающих и закрывающих скобок
-        if (balance == 0)
-            bracketCnt = true;
-
-        // определяем валидность строки с учётом всех критериев
-        if (numBracket && bracketCnt && !letterBracket && validCharacters && !emptyBrackets && !onlyDigits)
-            valid = true;
-        return valid;
+        // определяем равенство количества открывающих и закрывающих скобок, а также то, имеются ли в принципе скобки
+        return balance == 0 && bracketCnt != 0;
     }
 }
